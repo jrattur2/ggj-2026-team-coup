bf5831d2409925d28d0f187346861a5c18bc8ea0
a <a@as-MacBook-Pro.local>
2026-01-31 11:00:52 +0900

    add menu button translations

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -12,7 +12,7 @@ var current_language := GameLanguage.English
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	change_language(GameLanguage.English)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
