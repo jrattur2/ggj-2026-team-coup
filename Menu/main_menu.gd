c0ee9b3616723599aab2956420f704aa19fba5dd
a <a@MacBookPro.localdomain>
2026-01-31 18:55:28 +0900

    add music player, hook up volume sliders

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -4,16 +4,31 @@ signal on_main_menu_start_pressed()
 
 @onready var main_menu: MainMenu = $"."
 
+var master_bus_index := 0
+var music_bus_index := 0
+var sfx_bus_index := 0
+
 enum GameLanguage {
 	English,
 	Japanese,
 }
+
+@onready var master_slider: HSlider = $MarginContainer/VBoxContainer/MarginContainer2/VBoxContainer4/VBoxContainer/MasterSlider
+@onready var music_slider: HSlider = $MarginContainer/VBoxContainer/MarginContainer2/VBoxContainer4/VBoxContainer2/MusicSlider
+@onready var sfx_slider: HSlider = $MarginContainer/VBoxContainer/MarginContainer2/VBoxContainer4/VBoxContainer3/SFXSlider
 
 var current_language := GameLanguage.English
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	TranslationServer.set_locale("en")
+	master_bus_index = AudioServer.get_bus_index("Master")
+	master_slider.value = AudioServer.get_bus_volume_linear(master_bus_index)
+	music_bus_index = AudioServer.get_bus_index("Music")
+	music_slider.value = AudioServer.get_bus_volume_linear(music_bus_index)
+	sfx_bus_index = AudioServer.get_bus_index("SFX")
+	sfx_slider.value = AudioServer.get_bus_volume_linear(sfx_bus_index)
+	
 
 func change_language(new_language : GameLanguage):
 	if new_language == current_language:
@@ -34,10 +49,10 @@ func _on_ja_button_pressed() -> void:
 	change_language(GameLanguage.Japanese)
 
 func _on_master_slider_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	AudioServer.set_bus_volume_linear(master_bus_index, value)
 
 func _on_music_slider_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	AudioServer.set_bus_volume_linear(music_bus_index, value)
 
 func _on_sfx_slider_value_changed(value: float) -> void:
-	pass # Replace with function body.
+	AudioServer.set_bus_volume_linear(sfx_bus_index, value)
